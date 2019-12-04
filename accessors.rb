e861334a20e9cb83e2c6f64424d232cc2e24bbd6
tester <dmfrodol@yandex.ru>
2019-12-04 04:37:40 -0800

    try to understand stuff

--- accessors.rb
+++ accessors.rb
@@ -2,6 +2,28 @@
 
 module Accessors
   def attr_accessor_with_history(*names)
-    super.send(:attr_accessor, names)
+    names.each do |name|
+      message = 'Method should be a symbol!'
+      raise TypeError, message unless name.is_a? Symbol
+
+      var_name = "@#{name}".to_sym
+
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) do |v|
+        instance_variable_set(var_name, v)
+        instance_variable_set(var_name, v)
+      end
+
+      history = name.to_s + '_history'
+      var_history = "@#{name}_history".to_sym
+
+
+
+      # define_method(history_name) { instance_variable_get(history_name) }
+      # setter
+      # define_method("#{name}=") do |v|
+      #   instance_variable_set("@#{name}", v)
+      # end
+    end
   end
 end
--- test/accessors_spec.rb
+++ test/accessors_spec.rb
@@ -2,7 +2,19 @@ require 'rspec'
 require_relative '../accessors.rb'
 
 describe 'Accessors' do
-
+  before(:all) do
+    class TestClass
+      extend Accessors
+      attr_accessor_with_history :test
+    end
+  end
+  it 'should create reader for name and name_history' do
+    test_object = TestClass.new
+    test_object.test = 1
+    puts
+    puts test_object.instance_variables
+    puts test.
+    puts
+    # print(TestClass.instance_variables)
+  end
 end
-
-
