115f8a2ef4ff1bdb923b43f32ad4689690c5ec20
tester <dmfrodol@yandex.ru>
2019-12-05 23:57:07 -0800

    add method for strong typing

--- accessors.rb
+++ accessors.rb
@@ -3,8 +3,7 @@
 module Accessors
   def attr_accessor_with_history(*names)
     names.each do |name|
-      message = 'Method should be a symbol!'
-      raise TypeError, message unless name.is_a? Symbol
+      check_attr_name_is_symbol(name)
 
       var_name = "@#{name}".to_sym
       history = name.to_s + '_history'
@@ -13,17 +12,40 @@ module Accessors
       # setter and getter for attribute
       define_method(name) { instance_variable_get(var_name) }
 
-      define_method("#{name}=".to_sym) do |v|
-        instance_variable_set(var_name, v)
+      define_method("#{name}=".to_sym) do |value|
+        instance_variable_set(var_name, value)
         hist = instance_variable_get(var_history)
         if hist
-          instance_variable_set(var_history, hist.push(v))
+          instance_variable_set(var_history, hist.push(value))
         else
-          instance_variable_set(var_history, [v])
+          instance_variable_set(var_history, [value])
         end
       end
 
       define_method(history) { instance_variable_get(var_history) }
     end
+  end
+
+  def strong_attr_accessor(attr, attr_class)
+    check_attr_name_is_symbol(attr)
+
+    attr_sym = "@#{attr}".to_sym
+    define_method(attr) { instance_variable_get(attr_sym) }
+    define_method("#{attr}=".to_sym) do |value|
+      check_value_is_of_class(value, attr_class)
+      instance_variable_set(attr, value)
+    end
+  end
+
+  private
+
+  def check_attr_name_is_symbol(attr)
+    message = 'Method should be a symbol!'
+    raise TypeError, message unless attr.is_a? Symbol
+  end
+
+  def check_value_is_of_class(value, v_class)
+    message = "This variable should be a #{v_class}!"
+    raise TypeError, message unless value.is_a? v_class
   end
 end
