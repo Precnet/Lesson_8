194db9c2ef3d1ed352b82d1766af5a3bbb4b1b70
tester <dmfrodol@yandex.ru>
2019-12-06 01:53:47 -0800

    add methods according to task

--- validator.rb
+++ validator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'railway_error.rb'
 
 module Validator
@@ -7,24 +9,16 @@ module Validator
   end
 
   module ClassMethods
+    # attr_accessor :validations
+
+    def validations
+      @validations ||= {}
+    end
+
     def validate(attribute, validation_type, *params)
       check_validation_type(validation_type)
-    end
-
-    private
-
-    def check_validation_type(type)
-      message = "Validation type should be 'presence', 'format' or 'type'!"
-      raise TypeError, message unless %w[presence format type].include? type
-    end
-
-    def validate_presence(attribute)
-    end
-
-    def validate_format(attribute, format)
-    end
-
-    def validate_type(attribute, type)
+      @validations[validation_type] ||= []
+      @validations[validation_type].push([attribute, params])
     end
   end
 
@@ -32,8 +26,41 @@ module Validator
     def valid?
       validate!
       true
-    rescue RailwayError
+    rescue RailwayError => e
+      puts e.message
       false
+    end
+
+    def validate!
+      @@validations.each_key do |type|
+        @@validations[type].each do |params|
+          command = ('validate_' + type).to_sym
+          send(command, params)
+        end
+      end
+    end
+
+    private
+
+    def check_validation_type(type)
+      message = "Validation type should be 'presence', 'format' or 'type'!"
+      raise RailwayError, message unless %w[presence format type].include? type
+    end
+
+    def validate_presence(attribute)
+      message = "There is no attribute #{attribute} in class '#{self.class}'!"
+      has_attribute = self.class.instance_variables.include? attribute
+      raise RailwayError, message unless has_attribute
+    end
+
+    def validate_format(attribute, format)
+      message = "'#{attribute}' should be of format '#{format}'"
+      raise RailwayError, message unless attribute =~ format
+    end
+
+    def validate_type(attribute, type)
+      message = "'#{attribute}' should be '#{type}'!"
+      raise RailwayError, message unless attribute.is_a? type
     end
   end
 end
