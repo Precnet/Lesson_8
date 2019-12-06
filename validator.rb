725912a24c8ac392b909bd6706c8c1ede405026a
tester <dmfrodol@yandex.ru>
2019-12-06 00:50:51 -0800

    reformat add check for validation types

--- validator.rb
+++ validator.rb
@@ -1,28 +1,39 @@
 require_relative 'railway_error.rb'
 
 module Validator
-  def valid?
-    validate!
-    true
-  rescue RailwayError
-    false
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
   end
 
-  def validate(attribute, validation_type, *params)
+  module ClassMethods
+    def validate(attribute, validation_type, *params)
+      check_validation_type(validation_type)
+    end
 
+    private
+
+    def check_validation_type(type)
+      message = "Validation type should be 'presence', 'format' or 'type'!"
+      raise TypeError, message unless %w[presence format type].include? type
+    end
+
+    def validate_presence(attribute)
+    end
+
+    def validate_format(attribute, format)
+    end
+
+    def validate_type(attribute, type)
+    end
   end
 
-  private
-
-  def validate_presence(attribute)
-
-  end
-
-  def validate_format(attribute, format)
-
-  end
-
-  def validate_type(attribute, type)
-
+  module InstanceMethods
+    def valid?
+      validate!
+      true
+    rescue RailwayError
+      false
+    end
   end
 end
