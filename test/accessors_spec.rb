9dfcbf613d2e1e4072e9fe0723c1740341a3823c
tester <dmfrodol@yandex.ru>
2019-12-05 23:45:18 -0800

    add tests for variable creation

--- test/accessors_spec.rb
+++ test/accessors_spec.rb
@@ -5,16 +5,24 @@ describe 'Accessors' do
   before(:all) do
     class TestClass
       extend Accessors
-      attr_accessor_with_history :test
     end
+    @test_object = TestClass.new
   end
   it 'should create reader for name and name_history' do
-    test_object = TestClass.new
-    test_object.test = 1
-    puts
-    puts test_object.instance_variables
-    puts test.
-    puts
-    # print(TestClass.instance_variables)
+    expect(@test_object.instance_variables).to eq([])
+    TestClass.send(:attr_accessor_with_history, :smth, :test)
+    @test_object.smth = 1
+    expect(@test_object.instance_variables).to eq(%i[@smth @smth_history])
+    @test_object.test = '1234'
+    methods = %i[@smth @smth_history @test @test_history]
+    expect(@test_object.instance_variables).to eq(methods)
+  end
+  it 'should correctly track variable history' do
+    expect(@test_object.test_history).to eq(['1234'])
+    @test_object.test = 1234
+    expect(@test_object.test_history).to eq(['1234', 1234])
+    @test_object.test += 12
+    @test_object.test = nil
+    expect(@test_object.test_history).to eq(['1234', 1234, 1246, nil])
   end
 end
