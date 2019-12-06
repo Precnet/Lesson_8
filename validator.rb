0dc08486c93053d925d8fe92f656f61b3a2b0835
tester <dmfrodol@yandex.ru>
2019-12-06 00:23:44 -0800

    add stub for additional methods

--- validator.rb
+++ validator.rb
@@ -7,4 +7,22 @@ module Validator
   rescue RailwayError
     false
   end
+
+  def validate(attribute, validation_type, *params)
+
+  end
+
+  private
+
+  def validate_presence(attribute)
+
+  end
+
+  def validate_format(attribute, format)
+
+  end
+
+  def validate_type(attribute, type)
+
+  end
 end
